ac67afe55244c46990dd4a1037a395eb05440ecb
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-03-07 14:05:55 +0200

    Add:Data

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <>
       <div className="bg-[#1B262C]">
-        <div className="text-center text-[40px] text-white mb-[60px]">
+        <div className="text-center text-[40px] text-white mb-[70px]">
           <h2>
             Seeing the weather of the whole world <br />
             with <span className="text-[42px] text-[#5C93B1]"> Dark Weather!</span>
           </h2>
         </div>
-        <div className="flex justify-center items-center gap-6">
+        <div className="flex justify-center items-center gap-11 text-white">
           {WeatherDate.map((data) => {
             return <Card key={data.id} data={data} />;
           })}
--- src/components/utils/Cards.jsx
+++ src/components/utils/Cards.jsx
@@ -1,14 +1,14 @@
-export default function Cards({date}){
-  const {Country,degree,windSpeed,day,time,status ,image}= date;
+export default function Cards({data}){
+  const {country,degree,windSpeed,day,time,status ,image}= data;
   return(
     <>
-    <div className="card bg-[#24353E]  relative pt-[160px] pb- px-[50px] ">
-            <img  src={image} alt="" className="absolute top-[-60px] right-[-15px] w-[150px]"/>
-            <h3>
-              {Country} <span>{degree}</span>
+    <div className="card bg-[#24353E]  relative pt-[140px] pb-[25px] px-[50px] ">
+            <img  src={image} alt="" className="absolute top-[-60px] right-[-35px] w-[150px]"/>
+            <h3 className="text-[25px]">
+             <b> {country} <span>{degree}</span></b> 
             </h3>
             <p>wind speed : {windSpeed}</p>
-            <p>{day} : {time}</p>
+            <p>{day}  {time}</p>
             <p>{status} </p>
           </div>
     </>
